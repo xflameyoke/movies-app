15881110f6851a7fbd8a2dc76dfc1d9c59527920
xflameyoke <luczakmmateusz@gmail.com>
2022-02-13 17:45:34 +0100

    add: header in aboutpage

--- src/pages/aboutPage/aboutPage.jsx
+++ src/pages/aboutPage/aboutPage.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import StyledMain from './aboutPage.styled';
 import Header from './components/header/header';
-import { ImageGallery } from 'react-image-gallery';
-
-const images = [{ original: 'https://picsum.photos/id/1018/1000/600/' }];
 
 const AboutPage = () => (
   <StyledMain>
     <Header />
-    <ImageGallery items={images} />;
   </StyledMain>
 );
 
