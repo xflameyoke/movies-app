90cae7393cee5515ebc9571a5249f592ece45820
xflameyoke <luczakmmateusz@gmail.com>
2022-02-13 17:36:08 +0100

    full screen hero

--- src/pages/aboutPage/aboutPage.jsx
+++ src/pages/aboutPage/aboutPage.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import StyledMain from './aboutPage.styled';
+import Header from './components/header/header';
+import { ImageGallery } from 'react-image-gallery';
 
-const AboutPage = () => <StyledMain>About Page</StyledMain>;
+const images = [{ original: 'https://picsum.photos/id/1018/1000/600/' }];
+
+const AboutPage = () => (
+  <StyledMain>
+    <Header />
+    <ImageGallery items={images} />;
+  </StyledMain>
+);
 
 export default AboutPage;
---
+++ src/utils/urls.js
@@ -0,0 +1,4 @@
+export const url = {
+  background:
+    'https://images.unsplash.com/photo-1536514498073-50e69d39c6cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+};
